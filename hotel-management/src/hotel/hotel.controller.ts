ef54625100ae1e58aa717f7152e0d33fe1a4b9e1
HOSHICHEN7267 <antonychen5ds2@gmail.com>
2024-12-07 15:33:55 +0800

    Add upload file feature in hotel controller

--- hotel-management/src/hotel/hotel.controller.ts
+++ hotel-management/src/hotel/hotel.controller.ts
@@ -1,4 +1,16 @@
-import { Controller } from '@nestjs/common';
+import { Controller, Post, UploadedFile, UseInterceptors } from '@nestjs/common';
+import { FileInterceptor } from '@nestjs/platform-express';
+import { HotelService } from './hotel.service';
 
-@Controller('hotel')
-export class HotelController {}
+@Controller('hotels')
+export class HotelController {
+  constructor(private readonly hotelService: HotelService) {}
+
+  @Post('upload')
+  @UseInterceptors(FileInterceptor('file'))
+  async uploadFile(@UploadedFile() file: Express.Multer.File) {
+      console.log('File:', file);
+      await this.hotelService.importHotels(file.path);
+      return { message: 'Hotels imported successfully' };
+  }
+}
--- hotel-management/src/hotel/hotel.module.ts
+++ hotel-management/src/hotel/hotel.module.ts
@@ -1,9 +1,12 @@
 import { Module } from '@nestjs/common';
+import { TypeOrmModule } from '@nestjs/typeorm';
 import { HotelService } from './hotel.service';
 import { HotelController } from './hotel.controller';
+import { Hotel } from './hotel.entity';
 
 @Module({
+  imports: [TypeOrmModule.forFeature([Hotel])],
   providers: [HotelService],
-  controllers: [HotelController]
+  controllers: [HotelController],
 })
 export class HotelModule {}
